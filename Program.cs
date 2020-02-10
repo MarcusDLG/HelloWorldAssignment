277d0da327c3f422b62439f53dca1b7e923839c3
git config --global hub.protocol https <>
2020-02-10 15:06:28 -0500

    'created project!'

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldAssignment
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var myName = "Marcus";
+      Console.WriteLine("Hello, World, my name  is " + myName);
+    }
+  }
+}
